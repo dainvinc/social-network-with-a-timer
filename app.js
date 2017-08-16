fd248518c0cd16e83d326153b9fbb58011c5e51a
Vishal <vishal.koosuri1@marist.edu>
2017-08-16 16:01:56 +0000

    Pilot commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+// var request = require('request');
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server has started...");
+});
