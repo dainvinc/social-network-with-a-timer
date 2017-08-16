9b3b60d1f68fde3c92def7fbd4f51f58cec2895d
Vishal <vishal.koosuri1@marist.edu>
2017-08-16 19:12:02 +0000

    Included an array and populated the route

--- app.js
+++ app.js
@@ -4,8 +4,40 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+var friends = [{
+  name: "Cesare",
+  image: "http://dummyimage.com/114x170.jpg/ff4444/ffffff"
+}, {
+  name: "Candis",
+  image: "http://dummyimage.com/180x153.bmp/5fa2dd/ffffff"
+}, {
+  name: "Jannelle",
+  image: "http://dummyimage.com/202x234.png/dddddd/000000"
+}, {
+  name: "Drusie",
+  image: "http://dummyimage.com/234x162.jpg/cc0000/ffffff"
+}, {
+  name: "Aila",
+  image: "http://dummyimage.com/113x134.png/dddddd/000000"
+}, {
+  name: "Englebert",
+  image: "http://dummyimage.com/105x222.bmp/dddddd/000000"
+}, {
+  name: "Artemus",
+  image: "http://dummyimage.com/134x208.jpg/5fa2dd/ffffff"
+}, {
+  name: "Brianna",
+  image: "http://dummyimage.com/194x169.jpg/cc0000/ffffff"
+}, {
+  name: "Raynor",
+  image: "http://dummyimage.com/249x153.png/dddddd/000000"
+}, {
+  name: "Alec",
+  image: "http://dummyimage.com/195x124.bmp/5fa2dd/ffffff"
+}];
+
 app.get('/', function(req, res) {
-    res.render('home');
+    res.render('home', {friends: friends});
 });
 
 app.get('/friends', function(req, res) {
