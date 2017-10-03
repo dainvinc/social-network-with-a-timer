ddfc53a236e127366a6d350d559ebc649c884140
Vishal <vishal.koosuri1@marist.edu>
2017-10-03 17:02:08 +0000

    Included a new property in the schema

--- models/friends.js
+++ models/friends.js
@@ -4,7 +4,13 @@ var friendSchema = new mongoose.Schema({
     name: String,
     image: String,
     text: String,
-    created: {type: Date, default: Date.now}
+    created: {type: Date, default: Date.now},
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 });
 
 module.exports = mongoose.model("Friend", friendSchema);
