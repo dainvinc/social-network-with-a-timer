0e216178955bed5955e687ee8469acd7097073ed
Vishal <vishal.koosuri1@marist.edu>
2017-10-03 16:25:56 +0000

    Added new data to create more friends

--- seeds.js
+++ seeds.js
@@ -1,13 +1,31 @@
 var mongoose = require('mongoose');
 var Friend = require('./models/friends');
 
+var data = [
+    {
+        name: "First Post",
+        image: "https://images.pexels.com/photos/336413/pexels-photo-336413.jpeg?h=350&auto=compress&cs=tinysrgb",
+        text: "Create professional resumes, CV and bio-data online for free, in minutes. Simply fill in your details and generate beautiful PDF and HTML resumes!"
+        // created: {type: Date, default: Date.now}
+    }
+];
 function seedDB() {
     Friend.remove({}, function(err) {
         if(err) {
             console.log(err);
         }
         console.log("Cleaned up the database!");
-    });    
+    });
+    
+    data.forEach(function(seed) {
+        Friend.create(seed, function(err, data) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("New Friend added!");
+            }
+        });   
+    });
 }
 
 module.exports = seedDB;
