a5f1e4539e01b7db0864468ab9397ea699589128
Vishal <vishal.koosuri1@marist.edu>
2017-10-03 16:12:39 +0000

    Created a seeds file to clean up the database onload

---
+++ seeds.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Friend = require('.models/friends');
+
+function seedDB() {
+    Friend.remove({}, function(err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log("Cleaned up the database!");
+    });    
+}
